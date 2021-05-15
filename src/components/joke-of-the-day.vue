0c9833f6a111087381f34ea2b2305f5547864e3e
Takács <tamas.takacs1@vodafone.com>
2021-05-15 13:33:37 +0200

    random joke search

--- src/components/joke-of-the-day.vue
+++ src/components/joke-of-the-day.vue
@@ -6,7 +6,7 @@
         type="list-item-avatar-three-line"
       ></v-skeleton-loader>
     </div>
-    <v-row v-if="!loading" class="text-center">
+    <v-row v-if="!loading" class="text-center mb-10">
       <v-col cols="12">
         <div class="d-flex justify-center">
           <v-img :src="picURL" max-width="50" class="mb-2"></v-img>
@@ -17,11 +17,46 @@
         {{ joke }}
       </v-col>
     </v-row>
+    <div class="d-flex justify-center mt-10">
+      <v-card width="450">
+        <div class="d-flex flex-column align-center justify-center">
+          <v-card-title>Search for a joke</v-card-title>
+          <v-text-field
+            outlined
+            dense
+            label="Type something to search for"
+            placeholder="Type..."
+            v-model="search"
+            @keyup.enter="emitSearch"
+          ></v-text-field>
+          <v-btn
+            small
+            dark
+            color="green"
+            class="align-self-end ma-2"
+            @click="emitSearch"
+            >Make me laugh</v-btn
+          >
+          <div v-if="searchedJoke" class="pa-2 text-center text-subtitle-2">
+            {{ searchedJoke }}
+          </div>
+        </div>
+      </v-card>
+    </div>
   </v-container>
 </template>
 
 <script>
 export default {
-  props: ['joke', 'picURL', 'loading'],
+  props: ['joke', 'picURL', 'loading', 'searchedJoke'],
+  data: () => ({
+    search: null,
+  }),
+  methods: {
+    emitSearch() {
+      if (this.search == '') this.search = null
+      this.$emit('click', this.search)
+    },
+  },
 }
 </script>
--- src/views/Joke.vue
+++ src/views/Joke.vue
@@ -2,8 +2,10 @@
   <div class="joke">
     <joke-of-the-day
       :joke="joke"
+      :searchedJoke="searchedJoke"
       :picURL="url"
       :loading="loading"
+      @click="searchForJoke"
     ></joke-of-the-day>
   </div>
 </template>
@@ -15,16 +17,28 @@ export default {
     joke: null,
     url: null,
     loading: null,
+    searchedJoke: null,
   }),
   methods: {
     async getARandomJoke() {
       this.loading = true
       const response = await fetch('https://api.chucknorris.io/jokes/random')
       const extracted = await response.json()
-      console.log(extracted)
       this.joke = extracted.value
       this.url = extracted.icon_url
       this.loading = false
+    },
+    async searchForJoke(value) {
+      const response = await fetch(
+        `https://api.chucknorris.io/jokes/search?query=${value}`
+      )
+      const extracted = await response.json()
+      if (extracted.total == 0) {
+        this.searchedJoke =
+          "There is no joke for this search regarding Chuck Norris. It means this word probably doesn't exist."
+      } else {
+        this.searchedJoke = extracted.result[0].value
+      }
     },
   },
   mounted() {
